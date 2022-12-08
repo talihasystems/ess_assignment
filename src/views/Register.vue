680b3ba69e4abded3b1104518d5ae54a811dd629
Taliha Arif <taliha.arif@systemsltd.com>
2022-12-08 10:31:09 +0500

    register page set

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,32 +1,50 @@
+
+
 <template>
-    <v-container>
-     <div class="vue-tempalte">
-        <form @submit.prevent="userRegistration">
-            <h3>Sign Up</h3>
-            <div>
-                <label>Name</label>
-                <input type="text" v-model="user.name" />
-            </div>
-            <div>
-                <label>Email</label>
-                <input type="email" v-model="user.email" />
-            </div>
-            <div>
-                <label>Password</label>
-                <input type="password" v-model="user.password" />
-            </div>
-            <button type="submit">
-               Sign Up
-            </button>
-            <p>
-                Already registered 
-                <router-link :to="{name: 'login'}">sign in?</router-link>
-            </p>
-        </form>
-    </div>
-    </v-container>
-   
+  <div class="ContainerStyle">
+    <v-card class="signin">
+      <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+        @submit.prevent="userRegistration"
+        class="formStyle"
+      >
+      
+      <v-text-field
+        v-model="user.email"
+        :rules="emailRules"
+        label="E-mail"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="user.password"
+        type="password"
+        label="Password"
+        required
+      ></v-text-field>
+
+      
+      <v-btn
+        type="submit"
+        x-large
+        dark
+        style="margin-left: 30px"
+      >
+        Sign Up
+      </v-btn>
+
+      <p>
+          Already registered 
+          <router-link :to="{name: 'login'}">sign in?</router-link>
+      </p>
+
+    </v-form>
+    </v-card>
+  </div>
 </template>
+
 
 <script>
 import firebase from "firebase";
@@ -62,3 +80,28 @@ export default {
 };
 </script>
 
+
+<style scoped>
+.signin{
+  height: 400px;
+  /* margin: auto; */
+  position: absolute;
+  /* right: 4%; */
+  right: 4%;
+  top:25%;
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.ContainerStyle{
+  background: url('https://syshcm.systemsltd.com/EssPlus/login-bg.e69a8f472a164296cfd3.jpg') no-repeat center center fixed !important;
+  background-size: cover;
+  overflow: hidden;
+  height: 100vh
+}
+/* .formStyle{
+  padding: 200px 30px;
+} */
+</style>
